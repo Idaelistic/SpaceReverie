24e68949c75078ab9ae78e92d78d825960267953
Matheo PUEL <puel.matheo@gmail.com>
2023-10-24 16:31:15 +0200

    Add mailcatcher && send email async

--- readme.md
+++ readme.md
@@ -3,23 +3,27 @@
 # Inititalisation
 ```
 composer require webapp
+composer require symfony/mailer
 docker compose up
 php bin/console doctrine:migration:migrate
+
 # all .sql to fill the databse is in the sql file
 ```
 
 ```
 # Run the serveur
-php -S localhost:8000 -t public
-# APP: localhost:8000
+php -S localhost:8000 -t public or symfony server:start
 ```
+# APP
+- http://localhost:8000
 
 # Adminer:
-- URL: localhost:8888
+- URL: http://localhost:8888
 - SYSTEM: MySql
 - SERVER: database
 - USER: root
 - PW: root
 - DATABASE: spacereverie
 
-
+# MailCatcher
+- http://localhost:1025
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,32 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Email;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        return $this->render('index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/test', name: 'app_test')]
+    public function test(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to("coucou@example.com")
+            ->subject('Verify your email on Cauldron Overflow!')
+            ->text('Please, follow the link to verify your email!')
+            ->html(sprintf('Coucoucuocuidsqufiop'));
+        $mailer->send($email);
+
         return $this->render('index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
