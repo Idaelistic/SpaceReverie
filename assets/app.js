2bc27cb0f5640f9e8725eb3a1fa5fe098a3da8c6
Idaelistic <idaelistic@gmail.com>
2024-02-23 16:59:29 +0100

    Login register almost done

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+// import './bootstrap.js';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+
+import './styles/theme.min.css';
+import './styles/app.css';
+import './icons/around-icons.min.css'
+import './js/theme.min.js'
+import './js/test.js'
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ src/Entity/User.php
@@ -0,0 +1,254 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $username = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $display_name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $about_me = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $country = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $timezone = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $registered_at = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $registered_ip = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $registered_os = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $registered_browser = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getDisplayName(): ?string
+    {
+        return $this->display_name;
+    }
+
+    public function setDisplayName(string $display_name): static
+    {
+        $this->display_name = $display_name;
+
+        return $this;
+    }
+
+    public function getAboutMe(): ?string
+    {
+        return $this->about_me;
+    }
+
+    public function setAboutMe(?string $about_me): static
+    {
+        $this->about_me = $about_me;
+
+        return $this;
+    }
+
+    public function getCountry(): ?int
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?int $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getTimezone(): ?string
+    {
+        return $this->timezone;
+    }
+
+    public function setTimezone(string $timezone): static
+    {
+        $this->timezone = $timezone;
+
+        return $this;
+    }
+
+    public function getRegisteredAt(): ?\DateTimeImmutable
+    {
+        return $this->registered_at;
+    }
+
+    public function setRegisteredAt(\DateTimeImmutable $registered_at): static
+    {
+        $this->registered_at = $registered_at;
+
+        return $this;
+    }
+
+    public function getRegisteredIp(): ?string
+    {
+        return $this->registered_ip;
+    }
+
+    public function setRegisteredIp(string $registered_ip): static
+    {
+        $this->registered_ip = $registered_ip;
+
+        return $this;
+    }
+
+    public function getRegisteredOs(): ?string
+    {
+        return $this->registered_os;
+    }
+
+    public function setRegisteredOs(string $registered_os): static
+    {
+        $this->registered_os = $registered_os;
+
+        return $this;
+    }
+
+    public function getRegisteredbrowser(): ?string
+    {
+        return $this->registered_browser;
+    }
+
+    public function setRegisteredbrowser(string $registered_browser): static
+    {
+        $this->registered_browser = $registered_browser;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+}
